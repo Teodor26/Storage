49b1249ccc570216061ff1260d60dfe96323fd28
Teodor26 <teodorbear26@gmail.com>
2019-01-25 19:58:01 +0200

    Models were made.

---
+++ Storage.Models/DataModels/Arrival.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Models.DataModels
+{
+    public class Arrival
+    {
+
+        public int GoodId { get; set; }
+
+        public int Amount { get; set; }
+
+        DateTime date = new DateTime();
+    }
+}
---
+++ Storage.Models/DataModels/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Models.DataModels
+{
+    public class Purchase
+    {
+        public DateTime date = new DateTime();
+
+        public int GoodId { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
